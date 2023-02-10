78f69c089a7f811ebb3e6f04f4503fe522f29287
Andrei Warkentin <andrei.warkentin@intel.com>
2023-02-09 23:15:40 -0600

    fuzz: fix build and run issues
    At least on Linux!
    
    Also, build with -g. If the fuzzing reveals a bug, we'll need
    symbols to help debug it.
    
    Signed-off-by: Andrei Warkentin <andrei.warkentin@intel.com>

--- tests/fuzz/dlcorpus.sh
+++ tests/fuzz/dlcorpus.sh
@@ -1,9 +1,11 @@
 #/bin/sh
 #change to script directory
 cd `dirname $0`
-ls fuzz_emu*.c | sed 's/.c//' | while read target
+rm -rf corpus_*
+ls fuzz_emu*.c | sed 's/\.c//' | while read target
 do
     #download public corpus
+    rm -f public.zip
     wget "https://storage.googleapis.com/unicorn-backup.clusterfuzz-external.appspot.com/corpus/libFuzzer/unicorn_$target/public.zip"
     unzip -q public.zip -d corpus_$target
     #run target on corpus
