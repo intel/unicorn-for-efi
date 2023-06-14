2524b31413f3315b625a29f9caf7b746179c4fe2
Andrei Warkentin <andrei.warkentin@intel.com>
2023-06-14 17:37:17 -0500

    Squashed commit of the following:
    commit ebc122419b26c488ca49ead1bba04db3e895bd4d
    Author: Feng Han <feng1x.han@intel.com>
    Date:   Tue Jun 13 07:29:58 2023 +0000
    
        Fix -lto-unicorn library not found error
    
        Changed $(MODULE_DIR) to $(WORKSPACE), this enables other modules to
        include the unicorn package with lto library binary located correctly.
    
        Signed-Off-By: Feng Han <feng1x.han@intel.com>
    
    commit 08546b1629f1cb08a7ee8663ce108028db17056c
    Author: hanf2x <feng1x.han@intel.com>
    Date:   Tue Jun 6 09:47:35 2023 +0000
    
        Enable LTO for Unicorn AArch64
    
        This fixes the LTO error in aarch64, and also reduces lto-time on unicorn.
    
        GCC in LTO mode interoperates poorly with non-standard libraries that
        provide implementations of compiler intrinsics such as divti3/ashrti3
        or the stack protector entry points. Such libraries need to be built
        in non-LTO mode, and then referenced explicitly on the linker command
        line using a -plugin-opt=-pass-through=-lxxx linker option.
    
        The added liblto-unicorn library does not contain any code. Its only
        purpose is to pull in other libraries that implement intrinsics to which
        the linker's codegen pass may emit calls.
    
        The -flto=auto speeds up lto by utilizing all core threads which is supported by gcc.
    
        Signed-Off-By: Feng Han <feng1x.han@intel.com>
    
    Signed-off-by: Andrei Warkentin <andrei.warkentin@intel.com>

---
+++ efi/aarch64/liblto-unicorn.s
@@ -0,0 +1,12 @@
+## @file
+#
+#  Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+#
+##
+	.long	__divti3 - .
+	.long	__ashrti3 - .
